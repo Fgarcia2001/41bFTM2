ead4a127bac69acdca0bc6a791161c2a0b169d78
francog <franco2001garcia@gmail.com>
2023-08-10 00:41:28 -0300

    done

--- 08-React-Estado-LifeCycle/homework/01 - Exercises/src/components/Species/Species.jsx
+++ 08-React-Estado-LifeCycle/homework/01 - Exercises/src/components/Species/Species.jsx
@@ -1,7 +1,17 @@
 import React from "react";
-// import styledSpecies from "./Species.module.css";
+import styledSpecies from "./Species.module.css";
 
 export default function Species({ species, handleSpecies, handleAllSpecies }) {
-  // console.log(species);
-  return <div></div>
+  //console.log(species);
+  return (
+    <div>
+      <h2>Species</h2>
+      {species.map((specie, key) => (
+        <button key={key} onClick={handleSpecies} value={specie}>
+          {specie}
+        </button>
+      ))}
+      <button onClick={handleAllSpecies}>All Animals</button>
+    </div>
+  );
 }
