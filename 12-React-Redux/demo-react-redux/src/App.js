5df9793c7ba7efc5d5382df5e0d02d97041bc8e4
francog <franco2001garcia@gmail.com>
2023-08-16 03:35:38 -0300

    done

---
+++ 12-React-Redux/demo-react-redux/src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import CardsClass from "./components/CardsClass";
+import CardsFunc from "./components/CardsFunc";
+
+function App() {
+  return (
+    <div className="App">
+      <CardsClass />
+      <CardsFunc />
+    </div>
+  );
+}
+
+export default App;
---
+++ 12-React-Redux/demo-react-redux/src/redux/actions.js
@@ -0,0 +1,9 @@
+export const GET_CHARACTERS = "GET_CHARACTERS";
+
+export const getCharacters = () => {
+  return function (dispatch) {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((data) => dispatch({ type: GET_CHARACTERS, payload: data }));
+  };
+};
